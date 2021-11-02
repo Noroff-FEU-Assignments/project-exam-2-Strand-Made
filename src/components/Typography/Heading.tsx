103c44bf32b4352ab65061e00f1e712313782b41
stian <stianbekkelund@hotmail.com>
2021-11-02 20:13:09 +0100

    Update Heading.tsx

--- src/components/Typography/Heading.tsx
+++ src/components/Typography/Heading.tsx
@@ -1,36 +1,50 @@
 import { ReactNode } from "react";
 import styled from "styled-components";
-import { fontSize } from "../../utils/styleHelpers";
+import { HeroSection } from "../layout/Hero/Hero";
+import { fontSize, mediaQueries } from "../../utils/styleHelpers";
 import { fontSizes } from "../../globalStyle/_variables";
 
 interface HeadingProps {
   size?: keyof typeof fontSizes;
   color?: string;
+  weight?: "300" | "400" | "700";
   children: ReactNode;
 }
 const H1 = styled.h1<HeadingProps>`
   font-size: ${(props) => props.size && fontSize(props.size)};
   color: ${(props) => props.color};
+  font-weight: ${(props) => props.weight};
+  ${HeroSection} & {
+    font-family: "Abril Fatface", cursive;
+    color: var(--cool-gray-9);
+    letter-spacing: 1.3px;
+    text-align: center;
+  }
 `;
 const H2 = styled.h2<HeadingProps>`
   font-size: ${(props) => props.size && fontSize(props.size)};
   color: ${(props) => props.color};
+  font-weight: ${(props) => props.weight};
 `;
 const H3 = styled.h3<HeadingProps>`
   font-size: ${(props) => props.size && fontSize(props.size)};
   color: ${(props) => props.color};
+  font-weight: ${(props) => props.weight};
 `;
 const H4 = styled.h4<HeadingProps>`
   font-size: ${(props) => props.size && fontSize(props.size)};
   color: ${(props) => props.color};
+  font-weight: ${(props) => props.weight};
 `;
 const H5 = styled.h5<HeadingProps>`
   font-size: ${(props) => props.size && fontSize(props.size)};
   color: ${(props) => props.color};
+  font-weight: ${(props) => props.weight};
 `;
 const H6 = styled.h6<HeadingProps>`
   font-size: ${(props) => props.size && fontSize(props.size)};
   color: ${(props) => props.color};
+  font-weight: ${(props) => props.weight};
 `;
 
 const Heading = ({ size, color, children }: HeadingProps) => {
