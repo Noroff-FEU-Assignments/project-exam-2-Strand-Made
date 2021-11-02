5318731f736c03006da069004cd5989ea1fe9b31
stian <stianbekkelund@hotmail.com>
2021-11-02 20:14:30 +0100

    shadows and changed breakpoint

--- src/globalStyle/_variables.ts
+++ src/globalStyle/_variables.ts
@@ -1,16 +1,19 @@
 export const breakpoints = {
   xs: "480",
   sm: "768",
-  md: "992",
+  md: "952",
   lg: "1200",
 };
 
 export const shadows = {
-  sm: ``,
+  sm: `0 1px 3px rgba(0,0,0,.12), 0 1px 2px rgba(0,0,0,.24);`,
 
-  md: ` `,
+  md: `0 3px 6px rgba(0,0,0,.15), 0 2px 4px rgba(0,0,0,.12); `,
 
-  lg: `
+  lg: `0 10px 20px rgba(0,0,0,.15), 0 3px 6px rgba(0,0,0,.10);
+ `,
+
+  xl: `0 15px 25px rgba(0,0,0.15), 0 5px 10px rgba(0,0,0,.05);
  `,
 };
 
