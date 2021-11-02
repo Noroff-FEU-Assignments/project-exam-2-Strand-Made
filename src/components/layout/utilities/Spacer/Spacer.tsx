3be478b6a1985c1c1b50da2760c5043c00afa335
stian <stianbekkelund@hotmail.com>
2021-11-02 20:12:53 +0100

    Create Spacer.tsx

---
+++ src/components/layout/utilities/Spacer/Spacer.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+type marginTypes =
+  | "0.25"
+  | "0.5"
+  | "0.75"
+  | "1"
+  | "1.5"
+  | "2"
+  | "3"
+  | "4"
+  | "6"
+  | "8"
+  | "12"
+  | "16"
+  | "24"
+  | "32";
+
+interface SpacerProps {
+  mb?: marginTypes;
+  mt?: marginTypes;
+  m?: marginTypes;
+  mr?: marginTypes;
+  ml?: marginTypes;
+}
+
+const Spacer = styled.div<SpacerProps>`
+  margin-top: ${(props) => props.mt && `${props.mt}rem`};
+  margin-bottom: ${(props) => props.mb && `${props.mb}rem`};
+  margin-right: ${(props) => props.mr && `${props.mr}rem`};
+  margin-left: ${(props) => props.ml && `${props.ml}rem`};
+  margin: ${(props) => props.m && `${props.m}rem`};
+`;
+
+export default Spacer;
