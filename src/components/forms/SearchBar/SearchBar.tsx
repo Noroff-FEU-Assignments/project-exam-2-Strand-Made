bfcccb5a506853c637d73cd341fd810c73734c5f
stian <stianbekkelund@hotmail.com>
2021-11-02 20:13:37 +0100

    Update SearchBar.tsx

--- src/components/forms/SearchBar/SearchBar.tsx
+++ src/components/forms/SearchBar/SearchBar.tsx
@@ -1,32 +1,43 @@
 import styled from "styled-components";
 import FlexContainer from "../../layout/utilities/Flex/FlexContainer";
-import { borderRadius } from "../../../globalStyle/_variables";
+import { borderRadius, shadows } from "../../../globalStyle/_variables";
 import HotelInput from "./HotelInput";
 import DateInput from "./DateInput";
 import GuestInput from "./GuestInput";
-import Button from "../../Button/Button";
+import { PrimaryButton } from "../../Button/Button";
+import { mediaQueries } from "../../../utils/styleHelpers";
 
 const SearchHotel = styled.form`
   padding: 1.2rem;
   background: var(--teal-1);
   border-radius: ${borderRadius.md};
+  box-shadow: ${shadows.lg};
   width: 100%;
   max-width: 900px;
   margin: 0 auto;
   overflow-y: hidden;
 `;
 
+const Flex = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.75rem;
+  ${mediaQueries("sm")`
+flex-direction: row;
+`}
+`;
+
 const SearchBar = () => {
   return (
     <SearchHotel>
-      <FlexContainer col gap="0.75rem">
+      <Flex>
         <HotelInput />
 
         <DateInput />
 
         <GuestInput />
-        <Button> Search</Button>
-      </FlexContainer>
+        <PrimaryButton> Find Hotels</PrimaryButton>
+      </Flex>
     </SearchHotel>
   );
 };
