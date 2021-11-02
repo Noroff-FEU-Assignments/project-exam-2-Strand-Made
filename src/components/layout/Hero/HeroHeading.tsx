ade9d7da8575b04121fd5203a22a574bada96016
stian <stianbekkelund@hotmail.com>
2021-11-02 20:14:40 +0100

    Delete HeroHeading.tsx

--- src/components/layout/Hero/HeroHeading.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import Heading from "../../Typography/Heading";
-
-const HeroHeading = styled(Heading)`
-  font-family: "abril fatface", serif;
-  text-align: center;
-`;
-
-export default HeroHeading;
