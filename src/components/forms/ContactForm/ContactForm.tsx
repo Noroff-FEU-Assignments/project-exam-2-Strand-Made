fded78e99c1a7707808ed9d319dbbf6b792b683c
stian <stianbekkelund@hotmail.com>
2021-11-03 13:42:07 +0100

    Update ContactForm.tsx

--- src/components/forms/ContactForm/ContactForm.tsx
+++ src/components/forms/ContactForm/ContactForm.tsx
@@ -1,15 +1,29 @@
 import styled from "styled-components";
 import { borderRadius, shadows } from "../../../globalStyle/_variables";
+import { PrimaryButton } from "../../Button/Button";
 import Heading from "../../Typography/Heading";
-import { Input, Label, InputContainer } from "../SearchBar/HotelInput";
+import Label from "../Label/Label";
+import InputContainer from "../Input/InputContainer";
+import TextInput from "../Input/TextInput";
 
 const ContactBox = styled.div`
-  background: var(--teal-1);
+  background: var(--cool-gray-1);
   border-radius: ${borderRadius.md};
   padding: 1rem;
   max-width: 600px;
   margin: 0 auto;
   box-shadow: ${shadows.sm};
+`;
+
+const FormContainer = styled.div`
+  max-width: 400px;
+`;
+const TextBox = styled.textarea`
+  width: 100%;
+  background: var(--cool-gray-1);
+  border: 1px solid var(--cool-gray-2);
+  border-radius: ${borderRadius.sm};
+  padding: 0.5rem;
 `;
 
 const Form = styled.form``;
@@ -19,14 +33,21 @@ const ContactForm = () => {
     <ContactBox>
       <Heading size="3xl">Contact us</Heading>
       <Form>
-        <InputContainer>
-          <Label>Name</Label>
-          <Input type="text" />
-        </InputContainer>
-        <InputContainer>
-          <Label>Email</Label>
-          <Input type="text" />
-        </InputContainer>
+        <FormContainer>
+          <InputContainer>
+            <Label>Name</Label>
+            <TextInput type="text" />
+          </InputContainer>
+          <InputContainer>
+            <Label>Email</Label>
+            <TextInput type="text" />
+          </InputContainer>
+          <InputContainer>
+            <Label>Message</Label>
+            <TextBox />
+          </InputContainer>
+          <PrimaryButton>Submit</PrimaryButton>
+        </FormContainer>
       </Form>
     </ContactBox>
   );
