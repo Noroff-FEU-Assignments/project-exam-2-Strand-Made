974bda956141c8bf033c2001a49c1975ce0e1a3c
stian <stianbekkelund@hotmail.com>
2021-11-03 13:42:58 +0100

    button sizes added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,16 +6,32 @@ import Header from "../layout/Header/Header";
 interface ButtonProps {
   children: string;
   full?: boolean;
+  size?: keyof typeof buttonSizes;
+  href?: string;
 }
-
+const buttonSizes = {
+  sm: "8px 10px",
+  md: "12px 16px",
+  l: "15px 12px",
+};
 export const PrimaryButton = styled.button<ButtonProps>`
+  display: inline-block;
   background: var(--blue-5);
+  font-size: ${(props) => {
+    if (props.size === "sm") return "0.833rem";
+    if (props.size === "md") return "1rem";
+    if (props.size === "l") return "1.4rem";
+    return "1rem";
+  }};
   color: var(--blue-1);
   font-weight: 600;
-  letter-spacing: 0.8px;
   border-radius: 8px;
   width: ${(props) => props.full && "100%"};
-  padding: 0.5rem 1.5rem;
+  padding: ${(props) => {
+    if (props.size === "sm") return buttonSizes.sm;
+    if (props.size === "md") return buttonSizes.md;
+    if (props.size === "l") return buttonSizes.l;
+  }};
   box-shadow: ${shadows.md};
   :active,
   :focus {
@@ -35,4 +51,10 @@ export const SecondaryButton = styled(PrimaryButton)`
   background: none;
   color: var(--blue-5);
   box-shadow: none;
+  :active,
+  :focus {
+    background: none;
+    color: var(--blue-6);
+    box-shadow: none;
+  }
 `;
