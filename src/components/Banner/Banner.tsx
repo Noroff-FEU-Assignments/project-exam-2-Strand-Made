066311e9447b647afcd9d76d338dd84b623c1e8a
stian <stianbekkelund@hotmail.com>
2021-11-03 13:43:06 +0100

    refactored banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,62 +1,120 @@
 import styled from "styled-components";
 import {
-  HiOutlineChatAlt2,
-  HiOutlineWifi,
-  HiOutlineBadgeCheck,
-  HiOutlineEmojiHappy,
-} from "react-icons/hi";
+  AiFillSafetyCertificate,
+  AiFillWechat,
+  AiFillCheckCircle,
+} from "react-icons/ai";
+import { FaWifi } from "react-icons/fa";
 import { borderRadius } from "../../globalStyle/_variables";
-import FlexContainer from "../layout/utilities/Flex/FlexContainer";
-
-import Headings from "../Typography/Headings";
 import Heading from "../Typography/Heading";
+import { mediaQueries } from "../../utils/styleHelpers";
+import Spacer from "../layout/utilities/Spacer/Spacer";
 
-const BannerSection = styled.section`
+const BannerSection = styled.div`
   background: var(--teal-3);
   color: var(--teal-6);
   border-radius: ${borderRadius.md};
   padding: 1rem;
+  ${mediaQueries("md")`
+    padding: 1rem 4rem;
+  `}
 `;
 
-const BannerPromise = styled.div`
+const PromiseList = styled.ul`
   display: flex;
-  align-items: center;
-  gap: 0.5rem;
+  justify-content: center;
+  gap: 1.5rem;
+  flex-wrap: wrap;
+  padding: 1rem 0;
+  max-width: 500px;
 `;
 
+const Promise = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-weight: 700;
+  color: var(--teal-6);
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: var(--teal-2);
+  border-radius: 10000%;
+  height: 40px;
+  width: 40px;
+`;
+
+const BannerImageContainer = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
 const BannerImg = styled.img`
   border-radius: ${borderRadius.md};
+  width: 100%;
+
+  ${mediaQueries("md")`
+  height: 260px;
+  object-fit: cover;
+  `}
+`;
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: column;
+  ${mediaQueries("sm")`
+  flex-direction: row;
+`}
 `;
 
 const Banner = () => {
   return (
     <BannerSection>
-      <Headings>Our Promises</Headings>
-      <Heading.H2 size="xl"> This is a h2 Heading</Heading.H2>
-      <FlexContainer col>
-        <BannerPromise>
-          <HiOutlineChatAlt2 color={"var(--teal-5)"} size={24} />
-          <Headings as="h5">24/7 Contact</Headings>
-        </BannerPromise>
-        <BannerPromise>
-          <HiOutlineWifi color={"var(--teal-5)"} size={24} />
-          <Headings as="h5">24/7 Contact</Headings>
-        </BannerPromise>
-        <BannerPromise>
-          <HiOutlineBadgeCheck color={"var(--teal-5)"} size={24} />
-          <Headings as="h5">24/7 Contact</Headings>
-        </BannerPromise>
-        <BannerPromise>
-          <HiOutlineEmojiHappy color={"var(--teal-5)"} size={24} />
-          <Headings as="h5">24/7 Contact</Headings>
-        </BannerPromise>
-        <BannerImg
-          src={
-            "https://images.unsplash.com/photo-1570571054854-8f5a9f80504a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
-          }
-          alt="Woman relaxing in a comfy chair"
-        />
-      </FlexContainer>
+      <Heading.H3 weight="700" size="2xl">
+        Our Promise
+      </Heading.H3>
+      <Heading.H4 weight="400" size="l">
+        What differs us from the others?
+      </Heading.H4>
+      <Spacer mt="1" />
+      <Flex>
+        <PromiseList>
+          <Promise>
+            <IconContainer>
+              <AiFillCheckCircle size="32" />
+            </IconContainer>
+            <Heading.H5 size="md">Satisfaction guarantee</Heading.H5>
+          </Promise>
+          <Promise>
+            <IconContainer>
+              <FaWifi size="32" />{" "}
+            </IconContainer>
+            Free wifi at all locations
+          </Promise>
+          <Promise>
+            <IconContainer>
+              <AiFillSafetyCertificate size="32" />
+            </IconContainer>
+            All hosts are vetted
+          </Promise>
+          <Promise>
+            <IconContainer>
+              <AiFillWechat size="32" />
+            </IconContainer>
+            24/7 support
+          </Promise>
+        </PromiseList>
+        <BannerImageContainer>
+          <BannerImg
+            src={
+              "https://images.unsplash.com/photo-1570571054854-8f5a9f80504a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+            }
+            alt="Woman relaxing in a comfy chair"
+          />
+        </BannerImageContainer>
+      </Flex>
     </BannerSection>
   );
 };
