abcc9dc3bc9a9abbcfe2256ca8da6c12a1edf13e
stian <stianbekkelund@hotmail.com>
2021-11-03 13:43:59 +0100

    image responsiveness

--- src/components/SuggestionsCard/SuggestionsCard.tsx
+++ src/components/SuggestionsCard/SuggestionsCard.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { borderRadius } from "../../globalStyle/_variables";
+import { mediaQueries } from "../../utils/styleHelpers";
 import Heading from "../Typography/Heading";
 
 interface SuggestionProps {
@@ -10,18 +11,23 @@ interface SuggestionProps {
 
 const SuggestionContainer = styled.article`
   width: 100%;
+
+  max-width: 400px;
 `;
 const ImageContainer = styled.a``;
 const Image = styled.img`
   width: 100%;
   border-radius: ${borderRadius.md};
+  ${mediaQueries("md")`
+  height: 220px;
+  `}
 `;
 
 const SuggestionsCard = ({ title, img, imgDesc }: SuggestionProps) => {
   return (
     <SuggestionContainer>
       <ImageContainer>
-        <Image src={img} alt={imgDesc} />
+        <Image width="300" src={img} alt={imgDesc} />
       </ImageContainer>
       <Heading.H3 size="l">{title}</Heading.H3>
     </SuggestionContainer>
