287da74ae0c921b97bef64916a4a6d62e5196b3b
stian <stianbekkelund@hotmail.com>
2021-11-03 13:43:41 +0100

    spacer fixes

--- src/components/layout/utilities/Spacer/Spacer.tsx
+++ src/components/layout/utilities/Spacer/Spacer.tsx
@@ -24,12 +24,15 @@ interface SpacerProps {
   ml?: marginTypes;
 }
 
-const Spacer = styled.div<SpacerProps>`
+const Space = styled.div<SpacerProps>`
   margin-top: ${(props) => props.mt && `${props.mt}rem`};
   margin-bottom: ${(props) => props.mb && `${props.mb}rem`};
   margin-right: ${(props) => props.mr && `${props.mr}rem`};
   margin-left: ${(props) => props.ml && `${props.ml}rem`};
   margin: ${(props) => props.m && `${props.m}rem`};
 `;
+const Spacer = ({ mt, mb, mr, ml, m }: SpacerProps) => {
+  return <Space mt={mt} mb={mb} mr={mr} ml={ml} m={m} aria-hidden="true" />;
+};
 
 export default Spacer;
