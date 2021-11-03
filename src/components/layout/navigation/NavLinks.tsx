45c2188e47429afc2c5c2e315e5dc3121220e3d4
stian <stianbekkelund@hotmail.com>
2021-11-03 13:43:27 +0100

    active link styling

--- src/components/layout/navigation/NavLinks.tsx
+++ src/components/layout/navigation/NavLinks.tsx
@@ -5,6 +5,7 @@ import styled from "styled-components";
 
 const Link = styled(NavLink)<NavLinkProps>`
   display: block;
+  position: relative;
   padding: 1rem;
   border-radius: 8px;
   color: var(--cool-gray-5);
@@ -18,6 +19,19 @@ const Link = styled(NavLink)<NavLinkProps>`
     font-weight: 600;
     background: var(--cool-gray-2);
     color: var(--cool-gray-8);
+    ${mediaQueries("sm")`
+    ::after {
+      position: absolute;
+      height: 1px;
+      bottom: 10px;
+      left: 0;
+      right: 0;
+      width: 40px;
+      margin: 0 auto;
+      content: "";
+      border-bottom: 3px solid var(--cool-gray-8);
+    }
+    `}
   }
 
   ${mediaQueries("sm")`
