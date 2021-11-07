9766f7c9b490eb952864085a47e6ff9a3cec2c21
stian <stianbekkelund@hotmail.com>
2021-11-07 12:19:44 +0100

    searchbar styling

--- src/components/forms/SearchBar/SearchBar.tsx
+++ src/components/forms/SearchBar/SearchBar.tsx
@@ -1,13 +1,18 @@
 import styled from "styled-components";
-import FlexContainer from "../../layout/utilities/Flex/FlexContainer";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { HiOutlineLocationMarker } from "react-icons/hi";
 import { borderRadius, shadows } from "../../../globalStyle/_variables";
-import HotelInput from "./HotelInput";
-import DateInput from "./DateInput";
-import GuestInput from "./GuestInput";
+import InputContainer from "../Input/InputContainer";
+import FlexContainer from "../../layout/utilities/Flex/FlexContainer";
+import TextInput from "../Input/TextInput";
+import Label from "../Label/Label";
 import { PrimaryButton } from "../../Button/Button";
+import SearchResultList from "./SearchResults";
 import { mediaQueries } from "../../../utils/styleHelpers";
 
 const SearchHotel = styled.form`
+  position: relative;
   padding: 1.2rem;
   background: var(--teal-1);
   border-radius: ${borderRadius.md};
@@ -15,7 +20,6 @@ const SearchHotel = styled.form`
   width: 100%;
   max-width: 900px;
   margin: 0 auto;
-  overflow-y: hidden;
 `;
 
 const Flex = styled.div`
@@ -32,18 +36,54 @@ const ButtonContainer = styled.div`
 `;
 
 const SearchBar = () => {
+  const baseUrl = process.env.REACT_APP_BASE_URL;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [search, setSearch] = useState("");
+  const [hotels, setHotels] = useState([]);
+
+  useEffect(() => {
+    const getEstablishments = async () => {
+      try {
+        const res = await axios.get(`${baseUrl}/establishments`);
+
+        setHotels(res.data);
+      } catch (error) {
+        setError(true);
+      }
+    };
+    getEstablishments();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <SearchHotel>
       <Flex>
-        <HotelInput />
-
-        <DateInput />
-
-        <GuestInput />
+        <InputContainer>
+          <FlexContainer col>
+            <Label>
+              <HiOutlineLocationMarker color="var(--cool-gray-6)" size={25} />
+              <span>Hotel</span>
+            </Label>
+            <TextInput
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+              placeholder="Where are you staying?"
+            />
+          </FlexContainer>
+        </InputContainer>
         <ButtonContainer>
           <PrimaryButton size="md"> Search</PrimaryButton>
         </ButtonContainer>
       </Flex>
+      {error && <div>An error occured!</div>}
+      {search ? (
+        <SearchResultList
+          search={search}
+          setSearch={setSearch}
+          establishments={hotels}
+        />
+      ) : null}
     </SearchHotel>
   );
 };
