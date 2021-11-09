84c9fdb5d9bdfd3deabc8adf374555c9b48ab9d7
stian <stianbekkelund@hotmail.com>
2021-11-09 10:23:46 +0100

    refactored banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -5,61 +5,14 @@ import {
   AiFillCheckCircle,
 } from "react-icons/ai";
 import { FaWifi } from "react-icons/fa";
-import { borderRadius } from "../../globalStyle/_variables";
 import Heading from "../Typography/Heading";
 import { mediaQueries } from "../../utils/styleHelpers";
 import Spacer from "../layout/utilities/Spacer/Spacer";
-
-const BannerSection = styled.div`
-  background: var(--teal-3);
-  color: var(--teal-6);
-  border-radius: ${borderRadius.md};
-  padding: 1rem;
-  ${mediaQueries("md")`
-    padding: 1rem 4rem;
-  `}
-`;
-
-const PromiseList = styled.ul`
-  display: flex;
-  justify-content: center;
-  gap: 1.5rem;
-  flex-wrap: wrap;
-  padding: 1rem 0;
-  max-width: 500px;
-`;
-
-const Promise = styled.li`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  font-weight: 700;
-  color: var(--teal-6);
-`;
-
-const IconContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: var(--teal-2);
-  border-radius: 10000%;
-  height: 40px;
-  width: 40px;
-`;
-
-const BannerImageContainer = styled.div`
-  max-width: 600px;
-  margin: 0 auto;
-`;
-const BannerImg = styled.img`
-  border-radius: ${borderRadius.md};
-  width: 100%;
-
-  ${mediaQueries("md")`
-  height: 260px;
-  object-fit: cover;
-  `}
-`;
+import { IconContainer } from "../IconsContainer/IconsContainer";
+import BannerSection from "./BannerSection";
+import BannerList, { BannerListItem } from "./BannerList";
+import BannerImageContainer from "./BannerImgContainer";
+import BannerImg from "./BannerImg";
 
 const Flex = styled.div`
   display: flex;
@@ -80,32 +33,32 @@ const Banner = () => {
       </Heading.H4>
       <Spacer mt="1" />
       <Flex>
-        <PromiseList>
-          <Promise>
+        <BannerList>
+          <BannerListItem>
             <IconContainer>
               <AiFillCheckCircle size="32" />
             </IconContainer>
             <Heading.H5 size="md">Satisfaction guarantee</Heading.H5>
-          </Promise>
-          <Promise>
+          </BannerListItem>
+          <BannerListItem>
             <IconContainer>
               <FaWifi size="32" />{" "}
             </IconContainer>
             Free wifi at all locations
-          </Promise>
-          <Promise>
+          </BannerListItem>
+          <BannerListItem>
             <IconContainer>
               <AiFillSafetyCertificate size="32" />
             </IconContainer>
             All hosts are vetted
-          </Promise>
-          <Promise>
+          </BannerListItem>
+          <BannerListItem>
             <IconContainer>
               <AiFillWechat size="32" />
             </IconContainer>
             24/7 support
-          </Promise>
-        </PromiseList>
+          </BannerListItem>
+        </BannerList>
         <BannerImageContainer>
           <BannerImg
             src={
