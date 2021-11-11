8a7b4f8812d247dc69cb72a5d53f25d88884164e
stian <stianbekkelund@hotmail.com>
2021-11-11 12:08:12 +0100

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,12 @@
 import Heading from "../components/Typography/Heading";
+import Container from "../components/layout/Container/Container";
+import ContactForm from "../components/forms/ContactForm/ContactForm";
 
 const Contact = () => {
   return (
-    <div>
-      <Heading size="4xl">Contact us</Heading>
-    </div>
+    <Container>
+      <ContactForm />
+    </Container>
   );
 };
 
