67c680ceee89303d20ad07b0644e75335dc9b21d
stian <stianbekkelund@hotmail.com>
2021-11-12 23:15:02 +0100

    Update Box.tsx

--- src/components/layout/Box/Box.tsx
+++ src/components/layout/Box/Box.tsx
@@ -1,10 +1,11 @@
 import { ReactNode } from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { borderRadius } from "../../../globalStyle/_variables";
 
 interface IBox {
   children: ReactNode;
   padding?:
+    | "0"
     | "0.5rem"
     | "0.75rem"
     | "1rem"
@@ -14,6 +15,8 @@ interface IBox {
     | "4rem"
     | "5rem";
   borderRadius?: boolean;
+  borderRadiusT?: boolean;
+  borderRadiusB?: boolean;
   color?: string;
   background?: string;
 }
@@ -22,16 +25,44 @@ const StyledBox = styled.div<IBox>`
   --padding: ${(props) => props.padding};
   border-radius: ${(props) =>
     props.borderRadius ? `${borderRadius.md}` : null};
+  ${(props) => {
+    return (
+      props.borderRadiusT &&
+      css`
+        border-top-left-radius: ${borderRadius.md};
+        border-top-right-radius: ${borderRadius.md};
+      `
+    );
+  }}
+  ${(props) => {
+    return (
+      props.borderRadiusB &&
+      css`
+        border-bottom-left-radius: ${borderRadius.md};
+        border-bottom-right-radius: ${borderRadius.md};
+      `
+    );
+  }}
   padding: var(--padding);
   color: ${(props) => (props.color ? props.color : "inherit")};
   background: ${(props) => (props.background ? props.background : "inherit")};
 `;
 
-const Box = ({ children, padding, borderRadius, color, background }: IBox) => {
+const Box = ({
+  children,
+  padding,
+  borderRadius,
+  borderRadiusT,
+  borderRadiusB,
+  color,
+  background,
+}: IBox) => {
   return (
     <StyledBox
       padding={padding}
       borderRadius={borderRadius}
+      borderRadiusT={borderRadiusT}
+      borderRadiusB={borderRadiusB}
       color={color}
       background={background}
     >
