c230ae1862e7c190b5c814ba0ed9a8aff5d7b48e
stian <stianbekkelund@hotmail.com>
2021-11-12 23:14:54 +0100

    Update EnquireForm.tsx

--- src/components/forms/EnquireForm/EnquireForm.tsx
+++ src/components/forms/EnquireForm/EnquireForm.tsx
@@ -2,12 +2,13 @@ import styled from "styled-components";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import InputContainer from "../Input/InputContainer";
+import Stack from "../../layout/Stack/Stack";
 import TextInput from "../Input/TextInput";
 import Label from "../Label/Label";
 import TextBox from "../Input/TextBox";
 import { PrimaryButton } from "../../Button/Button";
 import Message from "../../Message/Message";
+import Box from "../../layout/Box/Box";
 
 const Form = styled.form`
   width: 350px;
@@ -32,43 +33,50 @@ const EnquireForm = () => {
   const onSubmit = (data) => console.log(data);
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
-      <InputContainer>
-        <Label htmlFor="firstName"> Your name </Label>
-        <TextInput
-          aria-invalid={errors.firstName ? "true" : "false"}
-          type="text"
-          id="firstName"
-          {...register("firstName")}
-        />
-        {errors.firstName && (
-          <Message.Error>{errors.firstName.message}</Message.Error>
-        )}
-      </InputContainer>
-      <InputContainer>
-        <Label htmlFor="email"> Your email </Label>
-        <TextInput
-          type="email"
-          aria-invalid={errors.email ? "true" : "false"}
-          id="email"
-          {...register("email")}
-        />
-        {errors.email && <Message.Error>{errors.email.message}</Message.Error>}
-      </InputContainer>
-      <InputContainer>
-        <Label htmlFor="message"> Enquiry </Label>
-        <TextBox
-          aria-invalid={errors.message ? "true" : "false"}
-          {...register("message")}
-        />
-        {errors.message && (
-          <Message.Error>{errors.message.message}</Message.Error>
-        )}
-      </InputContainer>
-      <InputContainer>
-        <PrimaryButton type="submit" size="md">
-          Submit
-        </PrimaryButton>
-      </InputContainer>
+      <Stack>
+        <Box>
+          <Label htmlFor="firstName"> Your name </Label>
+          <TextInput
+            aria-invalid={errors.firstName ? "true" : "false"}
+            type="text"
+            id="firstName"
+            placeholder="Name"
+            {...register("firstName")}
+          />
+          {errors.firstName && (
+            <Message.Error>{errors.firstName.message}</Message.Error>
+          )}
+        </Box>
+        <Box>
+          <Label htmlFor="email"> Your email </Label>
+          <TextInput
+            type="email"
+            placeholder="Email"
+            aria-invalid={errors.email ? "true" : "false"}
+            id="email"
+            {...register("email")}
+          />
+          {errors.email && (
+            <Message.Error>{errors.email.message}</Message.Error>
+          )}
+        </Box>
+        <Box>
+          <Label htmlFor="message"> Enquiry </Label>
+          <TextBox
+            placeholder="Additional details"
+            aria-invalid={errors.message ? "true" : "false"}
+            {...register("message")}
+          />
+          {errors.message && (
+            <Message.Error>{errors.message.message}</Message.Error>
+          )}
+        </Box>
+        <Box>
+          <PrimaryButton type="submit" size="md">
+            Submit
+          </PrimaryButton>
+        </Box>
+      </Stack>
     </Form>
   );
 };
