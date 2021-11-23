c27261cdc3e241515daacd5416a6adc5c9ebc18d
stian <stianbekkelund@hotmail.com>
2021-11-23 16:50:46 +0100

    refactored amenities

--- src/components/establishment/Amenities/Amenitites.tsx
+++ src/components/establishment/Amenities/Amenitites.tsx
@@ -9,6 +9,7 @@ import { borderRadius } from "../../../globalStyle/_variables";
 import { IconContainer } from "../../IconsContainer/IconsContainer";
 import FlexContainer from "../../layout/utilities/Flex/FlexContainer";
 import Heading from "../../Typography/Heading";
+import Box from "../../layout/Box/Box";
 
 const BannerBox = styled.div`
   background: var(--teal-2);
@@ -28,98 +29,65 @@ type TAmenities = {
 
 const Amenitites = ({ amenities }: TAmenities) => {
   const { breakfast, cleaning, gym, office, shower } = amenities;
+  let amenitiesArray = [
+    {
+      breakfast,
+      text: breakfast ? "Breakfast included" : null,
+    },
+    {
+      cleaning,
+      text: cleaning ? "Cleaning service" : null,
+    },
+    {
+      gym,
+      text: gym ? "Gym Included" : null,
+    },
+    {
+      office,
+      text: office ? "Office" : null,
+    },
+    {
+      shower,
+      text: shower ? "Bathroom" : null,
+    },
+  ];
+
+  const iconCheck = (amenity) => {
+    if (amenity.breakfast)
+      return <MdEmojiFoodBeverage color="var(--teal-6)" size={24} />;
+
+    if (amenity.shower) return <MdShower color="var(--teal-6)" size={24} />;
+
+    if (amenity.gym) return <MdSportsTennis color="var(--teal-6)" size={24} />;
+    if (amenity.cleaning)
+      return <MdOutlineCleaningServices color="var(--teal-6)" size={24} />;
+    if (amenity.office)
+      return <MdOutlineCleaningServices color="var(--teal-6)" size={24} />;
+  };
+
+  let removeNullArr = amenitiesArray.filter((ameni) => {
+    return ameni.text !== null;
+  });
 
   return (
-    <>
-      <FlexContainer gap="1.5rem" wrap="wrap">
-        {breakfast && (
-          <BannerBox>
+    <FlexContainer gap="1.5rem" wrap="wrap">
+      {removeNullArr.map((service, index) => {
+        return (
+          <BannerBox key={index}>
             <FlexContainer gap="0.5rem" alignItems="center">
-              <FlexContainer gap="0.25rem" alignItems="center">
-                <div>
-                  <IconContainer background="var(--cool-gray-1)">
-                    <MdEmojiFoodBeverage color="var(--teal-6)" size={24} />
-                  </IconContainer>
-                </div>
-                <div>
-                  <Heading.H5 size="md">Breakfast included</Heading.H5>
-                </div>
-              </FlexContainer>
+              <Box>
+                <IconContainer background="var--cool-gray-1">
+                  {iconCheck(service)}
+                </IconContainer>
+              </Box>
+              <Box>
+                <Heading.H5 size="md">{service.text}</Heading.H5>
+              </Box>
             </FlexContainer>
           </BannerBox>
-        )}
-        {shower && (
-          <BannerBox>
-            <FlexContainer gap="0.5rem" alignItems="center">
-              <FlexContainer gap="0.25rem" alignItems="center">
-                <div>
-                  <IconContainer background="var(--cool-gray-1)">
-                    <MdShower color="var(--teal-6)" size={24} />
-                  </IconContainer>
-                </div>
-                <div>
-                  <Heading.H5 size="md">Shower</Heading.H5>
-                </div>
-              </FlexContainer>
-            </FlexContainer>
-          </BannerBox>
-        )}
-        {gym && (
-          <BannerBox>
-            <FlexContainer gap="0.5rem" alignItems="center">
-              <FlexContainer gap="0.25rem" alignItems="center">
-                <div>
-                  <IconContainer background="var(--cool-gray-1)">
-                    <MdSportsTennis color="var(--teal-6)" size={24} />
-                  </IconContainer>
-                </div>
-                <div>
-                  <Heading.H5 size="md">Gym</Heading.H5>
-                </div>
-              </FlexContainer>
-            </FlexContainer>
-          </BannerBox>
-        )}
-        {cleaning && (
-          <BannerBox>
-            <FlexContainer gap="0.5rem" alignItems="center">
-              <FlexContainer gap="0.25rem" alignItems="center">
-                <div>
-                  <IconContainer background="var(--cool-gray-1)">
-                    <MdOutlineCleaningServices
-                      color="var(--teal-6)"
-                      size={24}
-                    />
-                  </IconContainer>
-                </div>
-                <div>
-                  <Heading.H5 size="md">Cleaning service</Heading.H5>
-                </div>
-              </FlexContainer>
-            </FlexContainer>
-          </BannerBox>
-        )}
-        {office && (
-          <BannerBox>
-            <FlexContainer gap="0.5rem" alignItems="center">
-              <FlexContainer gap="0.25rem" alignItems="center">
-                <div>
-                  <IconContainer background="var(--cool-gray-1)">
-                    <MdOutlineCleaningServices
-                      color="var(--teal-6)"
-                      size={24}
-                    />
-                  </IconContainer>
-                </div>
-                <div>
-                  <Heading.H5 size="md">Cleaning service</Heading.H5>
-                </div>
-              </FlexContainer>
-            </FlexContainer>
-          </BannerBox>
-        )}
-      </FlexContainer>
-    </>
+        );
+      })}
+    </FlexContainer>
   );
 };
 
