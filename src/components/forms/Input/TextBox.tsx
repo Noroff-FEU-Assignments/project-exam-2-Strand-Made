38bc162120f8237a9886bf1a5f4fca6897bc8d5d
stian <stianbekkelund@hotmail.com>
2021-11-24 16:26:11 +0100

    Update TextBox.tsx

--- src/components/forms/Input/TextBox.tsx
+++ src/components/forms/Input/TextBox.tsx
@@ -3,7 +3,7 @@ import { borderRadius } from "../../../globalStyle/_variables";
 
 const TextBox = styled.textarea`
   width: 100%;
-  background: var(--cool-gray-1);
+  background: white;
   border: 2px solid var(--cool-gray-2);
   border-radius: ${borderRadius.sm};
   padding: 0.5rem;
