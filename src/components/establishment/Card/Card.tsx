3235609a182b73a3d0e37b5c947fa5c7d5605568
stian <stianbekkelund@hotmail.com>
2021-12-05 13:38:33 +0100

    Opacity change and card link

--- src/components/establishment/Card/Card.tsx
+++ src/components/establishment/Card/Card.tsx
@@ -2,12 +2,12 @@ import styled from "styled-components/macro";
 import { Link } from "react-router-dom";
 import FlexContainer from "../../layout/utilities/Flex/FlexContainer";
 import Image from "../../layout/Image/Image";
-import Heading from "../../Typography/Heading";
 import Demphasize from "../../Typography/DeEmphasize";
 import Frame from "../../layout/utilities/Frame/Frame";
 import Box from "../../layout/Box/Box";
 import Stack from "../../layout/Stack/Stack";
 import Spacer from "../../layout/utilities/Spacer/Spacer";
+import Paragraph from "../../Typography/Paragraph";
 
 interface EstablishmentPropTypes {
   img: string;
@@ -17,43 +17,46 @@ interface EstablishmentPropTypes {
   altText?: string;
 }
 
-const EstablishmentCardImage = styled.div`
+const EstablishmentCard = styled(Link)`
+  color: inherit;
   transition-property: transform, opacity;
   transition-duration: 0.2s;
   transition-timing-function: ease-out;
   backface-visibility: hidden;
   :hover {
     transform: scale(1.02);
-    opacity: 0.8;
+    opacity: 0.9;
   }
 `;
 
+const EstablishmentCardImage = styled.div``;
+
 const Card = ({ img, title, price, slug, altText }: EstablishmentPropTypes) => {
   return (
-    <Box>
-      <EstablishmentCardImage>
-        <Link to={slug}>
+    <EstablishmentCard to={slug}>
+      <Box>
+        <EstablishmentCardImage>
           <Frame>
             <Image borderRadius src={img} alt={altText ? altText : title} />
           </Frame>
-        </Link>
-      </EstablishmentCardImage>
-      <Spacer mt="0.75" />
-      <FlexContainer justifyContent="space-between" alignItems="center">
-        <Heading.H2 weight="400" size="l">
-          {title}
-        </Heading.H2>
-        <Box>
-          <Stack>
-            <Demphasize>from</Demphasize>
-            <Heading.H3 size="l" color="var(--cool-gray-9)">
-              <Demphasize fontSize="1rem">$ </Demphasize> {price}
-              <Demphasize fontSize="1rem"> per night</Demphasize>
-            </Heading.H3>
-          </Stack>
-        </Box>
-      </FlexContainer>
-    </Box>
+        </EstablishmentCardImage>
+        <Spacer mt="0.75" />
+        <FlexContainer justifyContent="space-between" alignItems="center">
+          <Paragraph weight="600" size="l">
+            {title}
+          </Paragraph>
+          <Box>
+            <Stack>
+              <Demphasize>from</Demphasize>
+              <Paragraph weight="600" size="l" color="var(--cool-gray-9)">
+                <Demphasize fontSize="1rem">$ </Demphasize> {price}
+                <Demphasize fontSize="1rem"> per night</Demphasize>
+              </Paragraph>
+            </Stack>
+          </Box>
+        </FlexContainer>
+      </Box>
+    </EstablishmentCard>
   );
 };
 
