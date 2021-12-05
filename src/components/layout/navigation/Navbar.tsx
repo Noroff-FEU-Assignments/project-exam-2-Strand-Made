9465da805cc28922813bd099d856885579df6171
stian <stianbekkelund@hotmail.com>
2021-12-05 13:40:31 +0100

    fixed color bug on mobile

--- src/components/layout/navigation/Navbar.tsx
+++ src/components/layout/navigation/Navbar.tsx
@@ -73,7 +73,11 @@ const Navbar = () => {
             aria-expanded={isToggled ? "true" : "false"}
             onClick={setIsToggled}
           >
-            {isToggled ? <HiX size={24} /> : <HiMenu size={24} />}
+            {isToggled ? (
+              <HiX size={24} color="var(--cool-gray-9)" />
+            ) : (
+              <HiMenu size={24} color="var(--cool-gray-9)" />
+            )}
           </ToggleMenuBtn>
         </Nav>
         <ButtonContainer>
